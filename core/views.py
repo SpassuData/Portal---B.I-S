8e96983296d9a2350cf77a490ab636335ddcd5ae
MuriloDias12 <murilodiasdeoliveira>
2026-03-30 15:45:58 -0300

    ADD : V2 MVP

--- core/views.py
+++ core/views.py
@@ -1,13 +1,61 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {
-        'user': {
-            'displayName': 'Murilo Dias'
+    return render(request, 'home.html')
+
+
+def area(request, area):
+
+    areas = {
+        'rh': {
+            'titulo': 'Gestão de Jornada',
+            'cor': 'bg-orange-500',
+            'dashboards': [
+                {
+                    'nome': 'Horas Extras',
+                    'descricao': 'Análise de horas extras',
+                    'link': 'https://app.powerbi.com'
+                },
+                {
+                    'nome': 'Absenteísmo',
+                    'descricao': 'Faltas e atrasos',
+                    'link': 'https://app.powerbi.com'
+                }
+            ]
         },
-        'dashboards': {
-            'rh': 'https://app.powerbi.com/groups/16c484dd-1fde-44ee-857e-06627ee01d99/reports/0336e46b-d67c-4e9b-b5d4-b10471623cef/93cdeb5027554930ab01?experience=power-bi',
-            'fin': 'https://app.powerbi.com/groups/941e43fd-d3b7-467f-b090-d566237a3913/reports/a1cf9489-2c5a-466f-bae7-489de32fbe9c/a8fb0d499d72e68d054c?experience=power-bi',
-            'op': 'https://app.powerbi.com'
+
+        'fin': {
+            'titulo': 'Gestão Financeira',
+            'cor': 'bg-blue-800',
+            'dashboards': [
+                {
+                    'nome': 'DRE',
+                    'descricao': 'Demonstrativo de resultado',
+                    'link': 'https://app.powerbi.com'
+                },
+                {
+                    'nome': 'Fator K',
+                    'descricao': 'Indicadores estratégicos',
+                    'link': 'https://app.powerbi.com'
+                }
+            ]
+        },
+
+        'op': {
+            'titulo': 'Eficiência Operacional',
+            'cor': 'bg-green-500',
+            'dashboards': [
+                {
+                    'nome': 'Projetos',
+                    'descricao': 'Status dos projetos',
+                    'link': 'https://app.powerbi.com'
+                }
+            ]
         }
+    }
+
+    data = areas.get(area)
+
+    return render(request, 'area.html', {
+        'data': data
     })
